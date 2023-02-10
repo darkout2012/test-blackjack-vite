7bc5b9ba88b6e892aacee89a37b2691f543a15d1
Bogdan Bakhmat <darkout2012@gmail.com>
2023-02-10 09:14:22 +0100

    First commit

---
+++ main.js
@@ -0,0 +1,3 @@
+import "./src/blackjack/index.js" // poner tipo de extension .js es opcional y el propio index tmb es opcional, es decir, podriamos dejarlo asi - import "./src/blackjack
+
+import './style.css'
